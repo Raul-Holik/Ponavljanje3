16e612d7954d110f42dcff21fa45d1031b460398
Raul-Holik <113007938+Raul-Holik@users.noreply.github.com>
2023-09-11 11:47:42 +0200

    Add files via upload

---
+++ Ponavljanje4.D.3/Ponavljanje4.D.3/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ponavljanje4.D._3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int a=10, b=7;
+            double c=12.5, d=16.7;
+            double rez;
+
+            rez = (a + b + c + d) / 4;
+            Console.WriteLine(rez);
+            Console.ReadLine();
+        }
+    }
+}
